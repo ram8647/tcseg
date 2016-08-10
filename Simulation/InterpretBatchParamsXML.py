c129527535535b9ef15b372c5c27b5ca9b7f1fc6
jeremyadamsfisher <jeremy.adams.fisher@gmail.com>
2016-08-10 13:06:41 -0700

    Changed interpret xml logic so that, if r_grow is undefined, it will use a default value depending on whether there is any corresponding mitosis

--- Simulation/InterpretBatchParamsXML.py
+++ Simulation/InterpretBatchParamsXML.py
@@ -3,8 +3,6 @@ from itertools import product
 from collections import OrderedDict
 import ast
 
-# TODO: Add a feature to map certain variables onto other; so that r_mitosis 1 2 and 3 are synced, for example
-# TODO: add custom processing of certain variables, like r_grow
 
 def params_dict_for_batch(batch_iteration, xml_path, param_scan_specs_path):
     '''
@@ -97,10 +95,32 @@ def process_dictionary(dict):
         dict['r_mitosis_R3'] = val
         del dict['r_mitosis_R123']
 
+    if 'r_grow_R123' in dict.keys():
+        val = dict['r_grow_R123']
+        dict['r_grow_R1'] = val
+        dict['r_grow_R2'] = val
+        dict['r_grow_R3'] = val
+        del dict['r_grow_R123']
+
+    if 'r_mitosis_GZ' in dict.keys():
+        val = dict['r_mitosis_GZ']
+        dict['r_mitosis_R2'] = val
+        dict['r_mitosis_R3'] = val
+        del dict['r_mitosis_GZ']
+
+    if 'r_grow_GZ' in dict.keys():
+        val = dict['r_grow_GZ']
+        dict['r_grow_R2'] = val
+        dict['r_grow_R3'] = val
+        del dict['r_grow_GZ']
+
     for i in range(3):
-        if dict['r_mitosis_R{}'.format(i)] == [0.0, 0.0, 0.0]:
-            dict['r_grow_R{}'.format(i)] = [0.0, 0.0, 0.0]
+        default_growth_rate = 0.02
+        if dict['r_mitosis_R{}'.format(i)] == [0.0] * 3
+            dict['r_grow_R{}'.format(i)] = [0.0] * 3
         else:
-            dict['r_grow_R{}'.format(i)] = [0.05, 0.05, 0.05]
+            if not 'r_grow_R{}'.format(i) in dict:
+                dict['r_grow_R{}'.format(i)] = [default_growth_rate] * 3
+
 
     return dict
