2693b286840f41a09e69e0f6820b132bdc94ed08
jeremyadamsfisher <jeremy.adams.fisher@gmail.com>
2016-08-14 09:15:27 -0700

    Remove duplicate script to run compucell

--- BatchManager/BatchManagerScripts/RunCompuCell.py
+++
@@ -1,7 +0,0 @@
-from ModelIOManager import IOManager
-
-#compucell3d.bat –i Demos\cellsort_2D\cellsort_2D.xml –s screenshotDescription.sdfml –o Demos\cellsort_2D\screenshot
-
-def runCompuCell():
-    mngr = IOManager()
-
