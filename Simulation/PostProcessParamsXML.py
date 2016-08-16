cb796f609042003e5180fdccec1175c28eee5865
jeremyadamsfisher <jeremy.adams.fisher@gmail.com>
2016-08-15 18:11:59 -0700

    Add functionality to make sure that all neccisary variables are present and throws a descriptive error if not

--- Simulation/PostProcessParamsXML.py
+++ Simulation/PostProcessParamsXML.py
@@ -40,4 +40,26 @@ def process_dictionary(dict):
             if not 'r_grow_R{}'.format(i) in dict.keys():
                 dict['r_grow_R{}'.format(i)] = [default_growth_rate] * 3
 
+    error_check_params_dict(dict)
     return dict
+
+
+def error_check_params_dict(dict):
+    '''
+    Make sure that all parameters are specified and raise an error if they are not
+    '''
+    necessary_variables = ['embryo_size', 'stripe_period', 'dye_flag', 'x0_dye', 'xf_dye', 'y0_dye', 'yf_dye',
+                           'AP_growth_constraint_flag', 'forces_on', 'V_AP_GZposterior', 'k1_AP_GZanterior',
+                           'k2_AP_GZanterior', 'k1_AP_Segments', 'k2_AP_Segments', 'k1_ML_GZ', 'k2_ML_GZ',
+                           'k1_ML_Segments', 'k2_ML_Segments', 'mitosis_on', 'y_GZ_mitosis_border_percent',
+                           'mitosis_transition_times', 'r_mitosis_R0', 'r_mitosis_R1', 'r_mitosis_R2', 'r_mitosis_R3',
+                           'r_grow_R0', 'r_grow_R1', 'r_grow_R2', 'r_grow_R3', 'mitosis_fraction_AP_oriented',
+                           'mitosis_window', 'mitosis_Vmin_divide', 'mitosis_Vmax', 'mitosis_visualization_flag',
+                           'mitosis_visualization_window', 'dye_mitosis_clones', 'mitosis_dye_window']
+
+    missing_variables = []
+    for var in necessary_variables:
+        if var not in dict:
+            missing_variables.append(missing_variables)
+    if len(missing_variables) > 0:
+        raise NameError('Missing Variables! Please specify: {}',', '.join(missing_variables))
